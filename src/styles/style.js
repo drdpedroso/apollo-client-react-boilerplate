c5076921072c246ef6eef483095d92b1dd14b4d6
Eduardo Pedroso <drdpedroso@gmail.com>
2017-03-04 15:54:59 -0300

    First commit

---
+++ src/styles/style.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  '#app': {
+    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'string', 'value': 'auto' }, { 'unit': 'px', 'value': 0 }, { 'unit': 'string', 'value': 'auto' }]
+  }
+});
